c1b20a3ce52653525e0cf0de84076563fd446278
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2024-09-20 16:21:34 +0300

    create messages component for listing messages

---
+++ src/components/shared/Messages.tsx
@@ -0,0 +1,32 @@
+import { Message } from "@/app/actions";
+import { Bot, User } from "lucide-react";
+import * as React from "react";
+import { forwardRef } from "react";
+
+interface Props {
+  messages: Message[];
+}
+
+const Messages = forwardRef<HTMLDivElement, Props>(({ messages }, ref) => {
+  return (
+    <div ref={ref} className="flex flex-col gap-2">
+      {messages.map((message, index) => (
+        <div
+          className="text-white items-center flex gap-2 border-b border-[#27272a] pb-2 last:border-none last:pb-0"
+          key={index}
+        >
+          {message.role === "user" ? (
+            <User className="size-5 shrink-0 self-start" />
+          ) : (
+            <Bot className="size-5 shrink-0 self-start" />
+          )}
+          {message.content}
+        </div>
+      ))}
+    </div>
+  );
+});
+
+Messages.displayName = "Messages";
+
+export default Messages;
